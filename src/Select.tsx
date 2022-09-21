78a0bf7e2afe466526bb738d4f8a430904041e81
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-09-21 14:46:35 +0900

    mend

---
+++ src/Select.tsx
@@ -0,0 +1,30 @@
+import styles from "./select.module.css";
+
+type SelectOptions = {
+  label: string;
+  value: any;
+};
+
+type SelectProps = {
+  options: SelectOptions[];
+  value?: SelectOptions;
+  onChange: (value: SelectOptions | undefined) => void;
+};
+
+export function Select({ value, onChange, options }: SelectProps) {
+  return (
+    <div className={styles.container}>
+      <span className={styles.value}>Value</span>
+      <button className={styles["clear-btn"]}>&times;</button>
+      <div className={styles.divider} />
+      <div className={styles.caret} />
+      <ul className={styles.options}>
+        {options.map((opt) => (
+          <li key={opt.label} className={styles.option}>
+            {opt.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
