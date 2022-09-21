5e379537dcb9fd94e88c7f9ca2a604365969bdbb
Jon Gull <25947147+JonrGull@users.noreply.github.com>
2022-09-21 15:30:54 +0900

    add: create state for setting values

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { Select } from "./Select";
 
 const options = [
@@ -9,9 +11,15 @@ const options = [
 ];
 
 function App() {
+  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+
   return (
     <>
-      <Select options={options} />
+      <Select
+        options={options}
+        value={value}
+        onChange={(opt) => setValue(opt)}
+      />
     </>
   );
 }
